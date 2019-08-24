a348066efd17d480dc72ce1bae45999b16848eb0
Odintsov Timur <timodin@yandex.ru>
2019-08-24 11:13:26 +0300

    added exception handling

--- bars.py
+++ bars.py
@@ -4,26 +4,22 @@ from geopy import distance
 
 
 def load_data(filepath):
-    with open(filepath, 'r') as f:
-        return json.load(f)
+    with open(filepath, 'r') as file1:
+        return json.load(file1)
 
 
 def get_biggest_bar(bars_info):
     biggest_bar_info = max(
         bars_info['features'],
-        key=lambda bars: bars['properties']['Attributes']['SeatsCount']
-        )
-    biggest_bar_name = biggest_bar_info['properties']['Attributes']['Name']
-    return biggest_bar_name
+        key=lambda bars: bars['properties']['Attributes']['SeatsCount'])
+    return biggest_bar_info
 
 
 def get_smallest_bar(bars_info):
     smallest_bar_info = min(
         bars_info['features'],
-        key=lambda bar: bar['properties']['Attributes']['SeatsCount']
-        )
-    smallest_bar_name = smallest_bar_info['properties']['Attributes']['Name']
-    return smallest_bar_name
+        key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
+    return smallest_bar_info
 
 
 def get_closest_bar(bars_info, longitude, latitude):
@@ -32,22 +28,41 @@ def get_closest_bar(bars_info, longitude, latitude):
         bars_info['features'],
         key=lambda bar: distance.distance(
             my_coords,
-            bar['geometry']['coordinates']
-        ).km)
-    closet_bar_name = closet_bar_info['properties']['Attributes']['Name']
-    return closet_bar_name
+            bar['geometry']['coordinates']).km)
+    return closet_bar_info
 
 
 def main():
-    longitude = float(input('Введите вашу долготу: '))
-    latitude = float(input('Введите вашу широту: '))
-    bars_info = load_data('bars.json')
+    while True:
+        try:
+            filepath = input('Введите путь до файла: ')
+            bars_info = load_data(filepath)
+            break
+        except FileNotFoundError:
+            print('Такой файл не найден')
+        except json.decoder.JSONDecodeError:
+            print('Это файл не формата json')
+    while True:
+        try:
+            longitude = float(input('Введите вашу долготу: '))
+            break
+        except ValueError:
+            print('Введите числовое значение')
+    while True:
+        try:
+            latitude = float(input('Введите вашу широту: '))
+            break
+        except ValueError:
+            print('Введите числовое значение')
     biggest_bar = get_biggest_bar(bars_info)
+    biggest_bar_name = biggest_bar['properties']['Attributes']['Name']
     smallest_bar = get_smallest_bar(bars_info)
+    smallest_bar_name = smallest_bar['properties']['Attributes']['Name']
     closet_bar = get_closest_bar(bars_info, longitude, latitude)
-    print('Самый большой бар:', biggest_bar)
-    print('Самый маленький бар:', smallest_bar)
-    print('Самый близкий бар:', closet_bar)
+    closet_bar_name = closet_bar['properties']['Attributes']['Name']
+    print('Самый большой бар:', biggest_bar_name)
+    print('Самый маленький бар:', smallest_bar_name)
+    print('Самый близкий бар:', closet_bar_name)
 
 
 if __name__ == '__main__':
